5801cd8990e8e3fdf6220506e5af6b88aae5ee81
Nesay35 <nesrindemir35@gmail.com>
2023-10-19 00:07:59 +0300

    2023-10-18

---
+++ src/components/common/password-input.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { Form, InputGroup } from "react-bootstrap";
+import { BsEyeFill, BsEyeSlashFill } from "react-icons/bs";
+
+
+const PasswordInput = () => {
+  const [type, setType] = useState("password");
+  const handleType = () => {
+    const newType = type === "password" ? "text" : "password";
+    setType(newType);
+  };
+  return (
+    <InputGroup className="mb-3">
+      <Form.Control
+        type={type}
+        placeholder="Enter password"
+        aria-label="Enter password"
+        aria-describedby="basic-addon1"
+      />
+      <InputGroup.Text id="basic-addon1" onClick={handleType}>
+        {type === "password" ? <BsEyeFill /> : <BsEyeSlashFill/>}
+      </InputGroup.Text>
+    </InputGroup>
+  );
+};
+export default PasswordInput
